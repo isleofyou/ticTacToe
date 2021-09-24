20ec9368af53a47cacf9f7ec47d20625f300aa39
David Tran <frozenpho@gmail.com>
2021-09-23 21:51:47 -0600

    Update First player method

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
 class Game {
   constructor() {
-    this.moves = {};
+    this.players = [];
     }
-    checkTurn() {
-
+    addPlayers(player){
+      var newPlayer = new Player(player);
+      this.players.push(newPlayer);
+    }
+    determineFirstPlayer() {
+      var firstTurn = Math.floor(Math.random() * this.players.length)
+      this.players[firstTurn].isTurn = true;
     }
     checkForWin() {
-
+      console.log('hi');
     }
     checkForDraw() {
 
