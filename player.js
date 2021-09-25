a975a715fac4659a67443102878979363313c8ad
David Tran <frozenpho@gmail.com>
2021-09-24 21:10:50 -0600

    Create storage functions

--- player.js
+++ player.js
@@ -4,10 +4,14 @@ class Player {
       this.wins = 0;
       this.isTurn = false;
     }
-    saveWinToStorage() {
-
+    saveWinToStorage(player) {
+      var storagePlayer = JSON.stringify(player);
+      localStorage.setItem("thisGame", storagePlayer);
     }
     retrieveWinsFromStorage() {
-
+    var retrievedGame = localStorage.getItem("thisGame");
+    var parsedGame = JSON.parse(retrievedGame);
+    newGame.players[0] += retrievedGame.players[0].wins;
+    newGame.players[1] += retrievedGame.players[1].wins;
     }
 }
